7da5e0368563daa09d27b12588ed6ea26de739c1
jinwookwon-03 <jinwookwon03@gmail.com>
2026-03-11 01:19:05 +0900

    Add files via upload

---
+++ 2-10.py
@@ -0,0 +1,13 @@
+#1
+name = input('이름을 입력하세요 : ')
+print(name, '님이 입장하셨습니다.')
+
+#2
+m = int(input('숫자 m을 입력하세요 : '))
+n = int(input('숫자 n을 입력하세요 : '))
+print('m + n =', m + n)
+print('m - n =', m - n)
+
+#3
+r = int(input('원의 반지름을 정수로 입력하시오 : '))
+print(3.14 * r * r)
---
+++ 2-8.py
@@ -0,0 +1,7 @@
+a = 8 + 2j
+b = 4 + 3j
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
