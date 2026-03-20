7216073ed35ab469376041d5ed22e125d6b82f5c
jinwookwon-03 <jinwookwon03@gmail.com>
2026-03-20 11:33:54 +0900

    3rd Lecture

---
+++ clas.py
@@ -0,0 +1,276 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+============== RESTART: C:/Users/epfk6/Documents/AICoding/class.py =============
+num = 100
+
+============== RESTART: C:/Users/epfk6/Documents/AICoding/class.py =============
+num = 100
+num = 200
+
+============== RESTART: C:/Users/epfk6/Documents/AICoding/class.py =============
+num = 100
+num = 200
+num = 300
+
+============== RESTART: C:/Users/epfk6/Documents/AICoding/class.py =============
+num =  100
+num =  200
+num =  300
+
+============== RESTART: C:/Users/epfk6/Documents/AICoding/class.py =============
+num =  100
+num =  200
+num =  300
+
+============== RESTART: C:/Users/epfk6/Documents/AICoding/class.py =============
+num =  100
+num =  200
+num =  300
+num =  400
+num =  500
+num =  600
+num =  700
+num =  800
+num =  900
+num =  1000
+num =  1100
+num =  1200
+num =  1300
+num =  1400
+num =  1500
+num =  1600
+num =  1700
+num =  1800
+num =  1900
+num =  2000
+num =  2100
+num =  2200
+num =  2300
+num =  2400
+num =  2500
+num =  2600
+num =  2700
+num =  2800
+num =  2900
+num =  3000
+num =  3100
+num =  3200
+num =  3300
+num =  3400
+num =  3500
+num =  3600
+num =  3700
+num =  3800
+num =  3900
+num =  4000
+num =  4100
+num =  4200
+num =  4300
+num =  4400
+num =  4500
+num =  4600
+num =  4700
+num =  4800
+num =  4900
+num =  5000
+num =  5100
+num =  5200
+num =  5300
+num =  5400
+num =  5500
+num =  5600
+num =  5700
+num =  5800
+num =  5900
+num =  6000
+num =  6100
+num =  6200
+num =  6300
+num =  6400
+num =  6500
+num =  6600
+num =  6700
+num =  6800
+num =  6900
+num =  7000
+num =  7100
+num =  7200
+num =  7300
+num =  7400
+num =  7500
+num =  7600
+num =  7700
+num =  7800
+num =  7900
+num =  8000
+num =  8100
+num =  8200
+num =  8300
+num =  8400
+num =  8500
+num =  8600
+num =  8700
+num =  8800
+num =  8900
+num =  9000
+num =  9100
+num =  9200
+num =  9300
+num =  9400
+num =  9500
+num =  9600
+num =  9700
+num =  9800
+num =  9900
+num =  10000
+
+============== RESTART: C:/Users/epfk6/Documents/AICoding/class.py =============
+ith num =  0 100
+ith num =  1 200
+ith num =  2 300
+ith num =  3 400
+ith num =  4 500
+ith num =  5 600
+ith num =  6 700
+ith num =  7 800
+ith num =  8 900
+ith num =  9 1000
+ith num =  10 1100
+ith num =  11 1200
+ith num =  12 1300
+ith num =  13 1400
+ith num =  14 1500
+ith num =  15 1600
+ith num =  16 1700
+ith num =  17 1800
+ith num =  18 1900
+ith num =  19 2000
+ith num =  20 2100
+ith num =  21 2200
+ith num =  22 2300
+ith num =  23 2400
+ith num =  24 2500
+ith num =  25 2600
+ith num =  26 2700
+ith num =  27 2800
+ith num =  28 2900
+ith num =  29 3000
+ith num =  30 3100
+ith num =  31 3200
+ith num =  32 3300
+ith num =  33 3400
+ith num =  34 3500
+ith num =  35 3600
+ith num =  36 3700
+ith num =  37 3800
+ith num =  38 3900
+ith num =  39 4000
+ith num =  40 4100
+ith num =  41 4200
+ith num =  42 4300
+ith num =  43 4400
+ith num =  44 4500
+ith num =  45 4600
+ith num =  46 4700
+ith num =  47 4800
+ith num =  48 4900
+ith num =  49 5000
+ith num =  50 5100
+ith num =  51 5200
+ith num =  52 5300
+ith num =  53 5400
+ith num =  54 5500
+ith num =  55 5600
+ith num =  56 5700
+ith num =  57 5800
+ith num =  58 5900
+ith num =  59 6000
+ith num =  60 6100
+ith num =  61 6200
+ith num =  62 6300
+ith num =  63 6400
+ith num =  64 6500
+ith num =  65 6600
+ith num =  66 6700
+ith num =  67 6800
+ith num =  68 6900
+ith num =  69 7000
+ith num =  70 7100
+ith num =  71 7200
+ith num =  72 7300
+ith num =  73 7400
+ith num =  74 7500
+ith num =  75 7600
+ith num =  76 7700
+ith num =  77 7800
+ith num =  78 7900
+ith num =  79 8000
+ith num =  80 8100
+ith num =  81 8200
+ith num =  82 8300
+ith num =  83 8400
+ith num =  84 8500
+ith num =  85 8600
+ith num =  86 8700
+ith num =  87 8800
+ith num =  88 8900
+ith num =  89 9000
+ith num =  90 9100
+ith num =  91 9200
+ith num =  92 9300
+ith num =  93 9400
+ith num =  94 9500
+ith num =  95 9600
+ith num =  96 9700
+ith num =  97 9800
+ith num =  98 9900
+ith num =  99 10000
+>>> 
+============== RESTART: C:/Users/epfk6/Documents/AICoding/class.py =============
+나이를 입력하세요:
+...     ith num =  64 650030
+Traceback (most recent call last):
+  File "C:/Users/epfk6/Documents/AICoding/class.py", line 16, in <module>
+    age = int(input("나이를 입력하세요:"))
+ValueError: invalid literal for int() with base 10: ''
+>>> 
+>>> 
+============== RESTART: C:/Users/epfk6/Documents/AICoding/class.py =============
+나이를 입력하세요:15
+>>> 
+============== RESTART: C:/Users/epfk6/Documents/AICoding/class.py =============
+나이를 입력하세요:18
+청소년 할인
+>>> 
+>>> 
+============== RESTART: C:/Users/epfk6/Documents/AICoding/class.py =============
+나이를 입력하세요:21
+>>> 
+============== RESTART: C:/Users/epfk6/Documents/AICoding/class.py =============
+나이를 입력하세요:19
+청소년 할인
+>>> 
+============== RESTART: C:/Users/epfk6/Documents/AICoding/class.py =============
+나이를 입력하세요:25
+경로 우대
+>>> 
+>>> 19
+19
+>>> 
+============== RESTART: C:/Users/epfk6/Documents/AICoding/class.py =============
+나이를 입력하세요:
+
+============== RESTART: C:/Users/epfk6/Documents/AICoding/class.py =============
+나이를 입력하세요:19
+청소년 할인
+
+============== RESTART: C:/Users/epfk6/Documents/AICoding/class.py =============
+나이를 입력하세요:25
+
+============== RESTART: C:/Users/epfk6/Documents/AICoding/class.py =============
+나이를 입력하세요:50
+
+============== RESTART: C:/Users/epfk6/Documents/AICoding/class.py =============
+나이를 입력하세요:70
+경로 우대
