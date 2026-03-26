ce545c7ae92be55a94f3cea661b157b2ab59de20
jinwookwon-03 <jinwookwon03@gmail.com>
2026-03-27 01:30:56 +0900

    Add files via upload

---
+++ lab 5-2.py
@@ -0,0 +1,24 @@
+# lab 5-2 / 1
+prime_list = [2, 3, 5, 7]
+print('prime_list의 첫 원소 :', prime_list[0])
+
+# lab 5-2 / 2
+prime_list = [2, 3, 5, 7]
+print('prime_list의 마지막 원소 :', prime_list[3])
+
+# lab 5-2 / 3
+prime_list = [2, 3, 5, 7]
+print('prime_list의 마지막 원소 :', prime_list[-1])
+
+# lab 5-2 / 4
+nations = ['Korea', 'China', 'Russia', 'Malaysia']
+print('nations의 첫 원소 :', nations[0])
+
+# lab 5-2 / 5
+nations = ['Korea', 'China', 'Russia', 'Malaysia']
+print('nations의 마지막 원소 :', nations[-1])
+
+# lab 5-2 / 6
+nations = ['Korea', 'China', 'Russia', 'Malaysia']
+print('nations의 마지막 원소 :', nations[len(nations)-1])
+
---
+++ lab 5-6.py
@@ -0,0 +1,5 @@
+# lab 5-6 / 1
+list = [1, 2, 3]
+n = int(input('반복할 정수를 입력하시오 :'))
+print(n * list)
+
