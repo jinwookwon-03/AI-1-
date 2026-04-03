2e0c243bb29135779299f83838c7e1bb8f889996
jinwookwon-03 <jinwookwon03@gmail.com>
2026-04-03 11:44:24 +0900

    Add files via upload

---
+++ class 4.3-(4).py
@@ -0,0 +1,29 @@
+'''
+def root(a, b, c):
+    if -b+((b**2.0-4.0*a*c < 0:
+            return None, None
+    x1 = (-b+(b**2.0-4.0*a*c)**0.5)/(2.0*a)
+    x2 = (-b-(b**2.0-4.0*a*c)**0.5)/(2.0*a)
+    return(x1, x2)
+
+def root0(a, b, c):
+    if -b+(b**2.0-4.0*a*c < 0:
+           return None, None
+    else:
+        x1 = (-b+(b**2.0-4.0*a*c)**0.5)/(2.0*a)
+        x2 = (-b-(b**2.0-4.0*a*c)**0.5)/(2.0*a)
+        return x1,x2
+print(r1, r2)
+'''
+
+def print_sum():
+    a = 1
+    b = 2
+    result = a+ b
+    print('print_sum() 내부 :', a,  '과', b, '의 합은', result, '입니다.')
+
+a = 10
+b = 20
+print_sum()
+result = a + b
+print('print_sum() 내부 :', a, '과', b, '의 합은', result, '입니다.')
