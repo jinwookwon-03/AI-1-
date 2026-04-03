4da634b1a1b4c8a9dbbbee3aefe6bb4964cbcde7
jinwookwon-03 <jinwookwon03@gmail.com>
2026-04-03 11:13:25 +0900

    Add files via upload

---
+++ class 4.3-(3).py
@@ -0,0 +1,13 @@
+def iplus(x1, x2):
+    isum = x1 + x2
+    print(isum) #print는 아웃풋x
+    return isum #아웃풋
+
+hap = iplus(4, 5)
+print(hap)
+
+def print_star():
+    print('***********')
+
+print_star()
+print_star()
