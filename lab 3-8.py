9953d0385fa860ba80ca1e4d6aac3029520d4baf
jinwookwon-03 <jinwookwon03@gmail.com>
2026-04-07 01:47:23 +0900

    Add files via upload

--- lab 3-8.py
+++ lab 3-8.py
@@ -1,5 +1,19 @@
-# lab 3-9
-n = 7
-for i in range(n):
-    print(' ' *(n - 1 - i) + '#')
+# lab 3-8 / 1
+s = 0
+for i in range(1, 101, 1):
+    s = s + i
+print(s)
+
+# lab 3-8 / 2
+s = 0
+for i in range(0, 101, 2):
+    s = s + i
+print(s)
+
+# lab 3-8 / 3
+s = 0
+for i in range(1, 101, 2):
+    s = s + i
+print(s)
+
 
